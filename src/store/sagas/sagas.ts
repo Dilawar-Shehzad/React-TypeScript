940bcc62f0e606b78e348bf8d61c8babc017fcc4
Dilawar Shehzad (DS) <86353151+Dilawar-Shehzad@users.noreply.github.com>
2024-04-19 17:21:23 +0500

    new commit

---
+++ src/store/sagas/sagas.ts
@@ -0,0 +1,23 @@
+import { call, put } from 'redux-saga/effects';
+import api from '../../services/api';
+import { TodoListActionTypes } from '../../types';
+import { loadFailure, loadSuccess } from '../actions/items';
+
+export function* load() {
+  try {
+    const { data } = yield call(api.get, 'http://localhost:3000/items');
+    console.log('data',data);
+    
+    yield put(loadSuccess(data));
+  } catch (error) {
+    yield put(loadFailure());
+  }
+}
+
+export function* deleteRequest({ payload }: TodoListActionTypes) {
+  try {
+    yield call(api.delete, `http://localhost:3000/items/${payload.id}`);
+  } catch (error) {
+    console.log('DELETE ERROR');
+  }
+}
